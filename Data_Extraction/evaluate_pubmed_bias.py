b0f02b4ed7126093901638382f2db5b3bca83cd6
Chang <cbao12@cmc.edu>
2016-09-05 23:45:10 +0800

    More explicit error handling in evaluate_pubmed_bias.py

--- Data_Extraction/evaluate_pubmed_bias.py
+++ Data_Extraction/evaluate_pubmed_bias.py
@@ -21,8 +21,10 @@ def evaluate_bias_by_year():
         for country in rat:
             try:
                 total_rat_data_weight_for_year += rat[country][str(year)]
-            except:
+            except KeyError:
                 pass
+            except Exception, e:
+                print e
 
         # Evaluate SciMago data
         total_sci_mago_weight_for_year = 0.0
@@ -30,15 +32,21 @@ def evaluate_bias_by_year():
         for country in sci_mago:
             try:
                 total_sci_mago_weight_for_year += sci_mago[country][str(year)]
-            except:
+            except KeyError:
                 pass
+            except Exception, e:
+                print e
 
         for country in sci_mago:
             try:
                 pubmed_rat_to_sci_mago_ratio = (rat[country][str(year)]/total_rat_data_weight_for_year)/(sci_mago[country][str(year)]/total_sci_mago_weight_for_year)
                 pubmed_rat_to_sci_mago_bias[country][str(year)] = pubmed_rat_to_sci_mago_ratio
-            except Exception as e:
+            except KeyError:
                 pass
+            except ZeroDivisionError:
+                pass
+            except Exception, e:
+                print e
 
     output_file = "../Analysis/JSON_files/pubmed_rat_to_sci_mago_bias.json"
 
@@ -67,12 +75,16 @@ def evaluate_cumulative_bias():
         for year in xrange(2005, 2016):
             try:
                 rat_sum[country] += rat[country][str(year)]
-            except:
+            except KeyError:
                 pass
+            except Exception, e:
+                print e
             try:
                 sci_mago_sum[country] += sci_mago[country][str(year)]
-            except:
+            except KeyError:
                 pass
+            except Exception, e:
+                print e
 
     rat_total_sum = sum(rat_sum.values())
     sci_mago_total_sum = sum(sci_mago_sum.values())
@@ -80,20 +92,12 @@ def evaluate_cumulative_bias():
     for country in rat:
         try:
             pubmed_rat_to_sci_mago_bias[country] = (rat_sum[country]/rat_total_sum)/(sci_mago_sum[country]/sci_mago_total_sum)
-        except Exception as e:
+        except KeyError:
             pass
-
-# Uncomment the lines below to print countries sorted by bias factor
-#    rat_to_sci_mago_tuples = []
-
-#    for country in pubmed_rat_to_sci_mago_bias:
-#       try:
-#           rat_to_sci_mago_tuples.append((pubmed_rat_to_sci_mago_bias[country], country))
-#       except Exception as e:
-#           print e
-
-#    for tup in sorted(rat_to_sci_mago_tuples)[::-1][:50]:
-#       print tup[1], ":", tup[0]
+        except ZeroDivisionError:
+            pass
+        except Exception, e:
+            print e
 
     try:
         with open("../Analysis/JSON_files/pubmed_rat_to_sci_mago_bias_cumulative.json", "w") as bias:
@@ -122,8 +126,12 @@ def unbias_dengue_data():
         for year in xrange(2005, 2016):
             try:
                 bias_corrected_dengue_dict[country][str(year)] = dengue_dict[country][str(year)]/bias_dict[country][str(year)]
-            except Exception as e:
+            except KeyError:
                 pass
+            except ZeroDivisionError:
+                pass
+            except Exception, e:
+                print e
 
     try:
         with open("../Analysis/JSON_files/bias_corrected_dengue_fever_2005_2015.json", "w") as unbias:
